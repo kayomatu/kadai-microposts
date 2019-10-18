81e174cc294cf1a0907dbb28a4451b6a3561a13d
kayomatu <kayomatu29@gmail.com>
2019-10-18 11:40:46 +0000

    Lesson15課題2 お気に入り機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favorite_micropost, through: :favorites, source: :micropost
+  
   
   def follow(other_user)
     unless self == other_user
@@ -30,4 +33,18 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavorite(micropost)
+    favorites = self.favorites.find_by(micropost_id: micropost.id)
+    favorites.destroy if favorites
+  end
+  
+  def favorite?(micropost)
+    self.favorite_micropost.include?(micropost)
+  end
+  
 end
